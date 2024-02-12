f1c0bf1e613f6321fadd735da4e8ed2a274ca1ec
P. A Diluna Sithija Sulakshana <dilunasithija111@gmail.com>
2024-02-12 22:10:52 +0530

    Add print Ln command

--- src/git_new/Git_New.java
+++ src/git_new/Git_New.java
@@ -14,7 +14,7 @@ public class Git_New {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Diluna");
     }
     
 }
